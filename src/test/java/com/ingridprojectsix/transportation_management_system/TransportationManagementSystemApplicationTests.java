9618e5935543029337b62b27201addf5605cdfcc
unknown <bajidan4u@gmail.com>
2024-01-22 19:50:43 +0100

    started project

---
+++ src/test/java/com/ingridprojectsix/transportation_management_system/TransportationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingridprojectsix.transportation_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportationManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

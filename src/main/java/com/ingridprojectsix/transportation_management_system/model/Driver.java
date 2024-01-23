e6ee56bb067e80d811aab83691200467a4795e52
Jerry Antoni <93800419+Antonijerry@users.noreply.github.com>
2024-01-23 20:45:06 +0100

    added driver entity

--- src/main/java/com/ingridprojectsix/transportation_management_system/model/Driver.java
+++ src/main/java/com/ingridprojectsix/transportation_management_system/model/Driver.java
@@ -1,4 +1,54 @@
 package com.ingridprojectsix.transportation_management_system.model;
 
+import com.ingridprojectsix.transportation_management_system.dto.AvailabilityStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "drivers")
 public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "DriverId")
+    private Long driverId;
+
+    @OneToOne
+    @JoinColumn(name = "User_id", unique = true)
+    private User user;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "LicenceNumber", unique = true)
+    private String licenseNumber;
+
+    @Column(name = "PlateNumber", unique = true)
+    private String plateNumber;
+
+    @Column(name = "CarDetails")
+    private String carDetails;
+
+    @Column(name = "AvailabilityStatus")
+    @Enumerated(EnumType.STRING)
+    private AvailabilityStatus availabilityStatus;
+
+    @Column(name = "AccountBalance")
+    private Double accountBalance;
+
+    @Column(name = "RegistrationDate")
+    private LocalDateTime registrationDate;
+
+    @Column(name = "Location")
+    private String location;
 }
